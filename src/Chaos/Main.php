43520042cb51350ba8e50814da78b4e70499999a
Chaos <41303233+Chaqs@users.noreply.github.com>
2022-09-06 01:08:07 +0100

    Update Main.php

--- src/Chaos/Main.php
+++ src/Chaos/Main.php
@@ -4,6 +4,7 @@ namespace Chaos;
 use pocketmine\entity\animation\ArmSwingAnimation;
 use pocketmine\event\Listener;
 use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\server\DataPacketSendEvent;
 use pocketmine\network\mcpe\protocol\LevelSoundEventPacket;
 use pocketmine\plugin\PluginBase;
 
@@ -14,6 +15,19 @@ Class Main extends PluginBase implements Listener
     public function onEnable(): void
     {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+
+    public function onDataSend(DataPacketSendEvent $event): void
+    {
+        $packets = $event->getPackets();
+        foreach ($packets as $packet) {
+            if ($packet::NETWORK_ID === LevelSoundEventPacket::NETWORK_ID and $packet instanceof LevelSoundEventPacket) {
+                if ($packet->sound === LevelSoundEvent::ATTACK_NODAMAGE or $packet->sound === LevelSoundEvent::ATTACK_STRONG) {
+                    $event->cancel();
+                }
+            }
+        }
     }
 
     public function onDataPacketReceive(DataPacketReceiveEvent $event) : void{
